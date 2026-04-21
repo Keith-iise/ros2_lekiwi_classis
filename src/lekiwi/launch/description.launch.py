c6fc25fa134863476a9b6fd88d243ee207abe43c
Keith <2257761605@qq.com>
2026-04-21 10:18:01 +0800

    feat(lekiwi): 添加机器人描述文件的启动配置
    创建了description.launch.py启动文件，用于发布机器人状态和关
    节状态。该文件包含了机器人URDF模型的加载和状态发布的完整配
    置，并设置了joint_state_publisher参数以禁用GUI界面。
    ```

---
+++ src/lekiwi/launch/description.launch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # 路径配置
+    pkg_path = FindPackageShare('lekiwi')
+    urdf_path = PathJoinSubstitution([pkg_path, 'urdf', 'lekiwi_base_model.urdf'])
+     # 读取机器人描述（标准写法，不会报错）
+    robot_description = {
+        'robot_description': Command(['xacro ', urdf_path])
+    }
+
+    # 机器人状态发布
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[robot_description],
+    )
+
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'use_gui': False}],  # True 打开滑块调关节角度
+    )
+
+
+    return LaunchDescription([
+        robot_state_publisher,
+        joint_state_publisher,
+    ])
